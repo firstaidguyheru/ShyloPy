8b0460ac098006da21ca6bd971813de9c8d6fb66
clvrk <jheru711@Gmail.com>
2020-12-22 09:19:20 -0800

    (finally) got the event to work after adding the server member gateway intent

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.utils import get
 
 load_dotenv()
 
-intents = None
+intents = discord.Intents.members
 client = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('shylo!', 'shylo can you?', 'shylo'), help_comamnd = None, intents = intents)
 
 
@@ -26,17 +26,17 @@ async def hi_cmd(ctx, user: discord.User, *, msg):
         await ctx.send('Success!')
 
 ## ^ This command is to DM a user for example: *why* their discord bot wasn('t) added/removed onto the server, can be used for other reason's but im choosing to be mature in this case.
-{
-# @client.event
-#async def on_member_join(member):
-#    channel = get(member.guild.channels, name='welcome') ## specifying channel name.
- #   mbed = discord.Embed(
-  #      title = f'Welcome To The {member.guild.name}',
-   # )
-    #mbed.set_image(url=f'{member.avatar_url}')
-    #bed.set_footer(text=f'Members: {member.guild.member_count}')
-    #await channel.send(embed=mbed)
-}
-## ^ This event is used to welcome users to my server, id is specified.
+
+@client.event
+async def on_member_join(member):
+    channel = get(member.guild.channels, name='welcome') ## specifying channel name.
+    mbed = discord.Embed(
+        title = f'Welcome To {member.guild.name} | [Invite Link](https://discord.gg/csUnYsr)',
+    )
+    mbed.set_image(url=f'{member.avatar_url}')
+    mbed.set_footer(text=f'New Member Count: {member.guild.member_count}')
+    await channel.send(embed=mbed)
+
+## ^ This event is used to welcome users to my server, server members intent needed for it to work.
 
 client.run(os.getenv('TOKEN'))
