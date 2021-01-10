69399addbda6ac08725a720f1c1949c1ecb6f2d6
clvrk <jheru711@Gmail.com>
2021-01-10 17:08:22 -0400

    Added more to welcome message.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import discord
 from asyncio import sleep as s
 from discord.utils import get
-
+from asyncio import sleep
 load_dotenv()
 
 intents = discord.Intents.all()
@@ -20,6 +20,7 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     channel = get(member.guild.channels, name='welcome') ## specifying channel name.
+    channel_2 = get(member.guild.channels, name='general')
     mbed = discord.Embed(
         title = f'Welcome To {member.guild.name}, {member.mention}',
         url = 'https://discord.gg/csUnYsr',
@@ -27,7 +28,17 @@ async def on_member_join(member):
     )
     mbed.set_image(url=f'{member.avatar_url}')
     mbed.set_footer(text=f'New Member Count: {member.guild.member_count} | ID: {member.id}')
+    mbed_2 = discord.Embed(
+        description = f'{member.mention} hopped into the Chamber. <:readthedocs:775801469685071893>',
+        color = 0x2c2f33
+    )
+    mbed_2.set_footer(text=f'New Member Count: {member.guild.member_count}')
     await channel.send(embed=mbed)
+    await channel_2.send(embed=mbed_2, delete_after=60*60)
+    await sleep(1)
+    for channel_3 in member.guild.channels:
+        if channel_3.name.startswith('N'):
+            await channel_3.edit(name=f'Null: {member.guild.member_count}')
 
 ## ^ This event is used to welcome users to my server, server members intent needed for it to work.
 
@@ -48,6 +59,8 @@ async def on_message(message):
         await message.channel.send('<:readthedocs:775801469685071893>')
     elif 'i have a question' in message.content:
         await message.channel.send('<:readthedocs:775801469685071893>')
+    elif 'how' in message.content:
+        await message.channel.send('<:readthedocs:775801469685071893>')
 
 ## ^ Read the docs | Switch Cases in python!
 
@@ -62,11 +75,11 @@ async def reply(ctx, user: discord.User, *, msg): # placing in args needed for s
 
 # notifier for modmail.
 
-extensions = ['Cogs.videos', 'Cogs.modmail']
+extensions = ['Cogs.additional', 'Cogs.modmail']
 
 if __name__ == '__main__':
     for ext in extensions:
         client.load_extension(ext)
 
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv("TOKEN"))
