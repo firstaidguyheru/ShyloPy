6037651b5667b4c791e8d9385dc6a6e584fd75bc
clvrk <52180876+clvrk@users.noreply.github.com>
2021-02-06 20:11:44 -0800

    back to old check....

--- Cogs/modmail.py
+++ Cogs/modmail.py
@@ -17,8 +17,9 @@ class modmail(commands.Cog):
         channels = self.bot.get_all_channels()
         channel = get(channels, guild__name="Clark's Chamber", name='staff-chat')
         try:
-             await channel.send(message.content + f"\n{message.author}[`{message.author.id}`]")
-             await message.channel.send('Your message has been sent!', delete_after=7)
+            if message.channel == message.author.dm_channel:
+                await channel.send(message.content + f"\n{message.author}[`{message.author.id}`]")
+                await message.channel.send('Your message has been sent!', delete_after=7)
         except:
             pass
     ## ^ simple modmail event/function, it works, that's all that matters.
