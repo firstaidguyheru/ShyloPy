1270b707d1c43b1bee9a16ed1e105506fbbed9a6
Heru J <jheru711@gmail.com>
2021-04-01 16:49:18 -0400

    Remove id check for monke

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def on_message(message):
 
     else:
         channel = get(client.get_all_channels(), guild__name="Clark's Chamber", name='monke-chain')
-        if message.channel.id == channel.id:
+        if message.channel == channel:
             if not message.content == 'monke'.casefold():
                 await message.delete()
             else:
