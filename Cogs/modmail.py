bd708f6a9494e17d734c89284cbb2b521af9aa50
Heru J <jheru711@gmail.com>
2021-04-09 15:34:20 -0400

    test

--- Cogs/modmail.py
+++ Cogs/modmail.py
@@ -18,25 +18,25 @@ class Modmail(commands.Cog):
         
         if message.author.bot:
             return
-        try:
+        
+        else:
             if message.channel == message.author.dm_channel:
                 time_difference = (datetime.utcnow() - self.last_timeStamp).total_seconds()
-                
+
                 if time_difference < 5:
                     return await message.channel.send("You are on cooldown!")
-                
+
                 embed = discord.Embed(
                     title = f"Modmail From `{message.author}`", 
                     description = f"{message.content}", 
                     color = 0x2c2f33
                 )
                 embed.set_footer(text=f'ID: {message.author.id}')
-                
+
                 await self.modmail_channel.send(embed=embed)
                 await message.channel.send('Your message has been sent!', delete_after = 7)
                 self.last_timeStamp = datetime.utcnow()
-        except:
-            pass
+        
 
 def setup(bot):
     bot.add_cog(Modmail(bot))
