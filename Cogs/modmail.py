73b527befe049337f04d5a4a053404388f206ce2
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2021-04-09 14:11:12 -0400

    updated modmail

--- Cogs/modmail.py
+++ Cogs/modmail.py
@@ -1,42 +1,42 @@
-import discord as d
-import random
+import discord
 from discord.ext import commands
-from discord.utils import get
-from discord.ext.commands import BucketType
-import datetime
-import time
+from datetime    import datetime
 
 
-class modmail(commands.Cog):
+class Modmail(commands.Cog):
+    
     def __init__(self, bot):
         self.bot = bot
-        self.last_timeStamp = datetime.datetime.utcfromtimestamp(0)
+        self.channel_id = 795663906018033735
+        self.modmail_channel = self.bot.get_channel(self.channel_id)
+        self.last_timeStamp  = datetime.datetime.utcfromtimestamp(0)
 
     @commands.Cog.listener()
     @commands.dm_only()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
+        """simple modmail event, it works, that's all that matters."""
+        
         if message.author.bot:
             return
-        channels = self.bot.get_all_channels()
-        channel = get(channels, guild__name="Clark's Chamber", name='staff-chat')
         try:
             if message.channel == message.author.dm_channel:
-                time_difference = (datetime.datetime.utcnow() - self.last_timeStamp).total_seconds()
+                time_difference = (datetime.utcnow() - self.last_timeStamp).total_seconds()
+                
                 if time_difference < 5:
-                    await message.channel.send("You are on cooldown!")
-                    return
-                await channel.send(embed=d.Embed(title=f'Modmail From {message.author}', color=0x2c2f33, description=f"{message.content}").set_footer(text=f'ID: {message.author.id}'))
-                await message.channel.send('Your message has been sent!', delete_after=7)
-                self.last_timeStamp = datetime.datetime.utcnow()
+                    return await message.channel.send("You are on cooldown!")
+                
+                embed = discord.Embed(
+                    title = f"Modmail From `{message.author}`", 
+                    description = f"{message.content}", 
+                    color = 0x2c2f33
+                )
+                embed.set_footer(text=f'ID: {message.author.id}'))
+                
+                await self.modmail_channel.send(embed=embed)
+                await message.channel.send('Your message has been sent!', delete_after = 7)
+                self.last_timeStamp = datetime.utcnow()
         except:
             pass
 
-    ## ^ simple modmail event, it works, that's all that matters.
-
-
-
-
-
-
 def setup(bot):
-    bot.add_cog(modmail(bot))
+    bot.add_cog(Modmail(bot))
