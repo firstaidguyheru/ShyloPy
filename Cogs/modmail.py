9614b00b84f595bee0ea3a5fda001ba4b19cbdcd
Heru J <jheru711@gmail.com>
2021-04-26 17:16:27 -0400

    Reverted cooldown to datetime

--- Cogs/modmail.py
+++ Cogs/modmail.py
@@ -1,12 +1,12 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import BucketType
+from datetime import datetime
 
 class Modmail(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
-        self.cooldown = commands.CooldownMapping.from_cooldown(1, 5, commands.BucketType.member)
+        self.last_timeStamp = datetime.utcfromtimestamp(0)
 
     @commands.Cog.listener()
     @commands.dm_only()
@@ -18,8 +18,10 @@ class Modmail(commands.Cog):
         
         else:
             if message.channel == message.author.dm_channel:
-                if self.cd_mapping.get_bucket(message).update_rate_limit():
-                    return await ctx.send("You are on cooldown.")
+                time_difference = (datetime.utcnow() - self.last_timeStamp).total_seconds()
+
+                if time_difference < 5:
+                    return await message.channel.send("You are on cooldown!")
                 
                 self.channel_id = 795663906018033735
                 self.modmail_channel = self.bot.get_channel(self.channel_id)
